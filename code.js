6e2b095a215d1ab83e259f9be079a0ccb6f47396
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-08 10:49:13 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function permutationSort(a) {
-    return 0;
+    sort(a, 0, a.length -1);
+    return a;
+}
+
+function sort(array, low, high) {
+    var iter, pivot;
+    if (lo >= hi) return;
+    p = lo;
+    for (i = lo +1; i <= hi; i++)
+        if (array[iter] < array[lo])
+            var swap = array[++p];
+            array[p] = array[i];
+            array[i] = swap;
+    swap = array[low];
+    array[low] = array[p];
+    array[p] = swap;
+    sort(array, lo, p-1);
+    sort(array, p+1, hi);
 }
